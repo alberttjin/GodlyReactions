9eee1009cf495ee53a82dc0a7c00aa49d04003a8
alberttjin <albertjin@berkeley.edu>
2017-06-07 19:57:39 -0700

    working on local host

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var server = require("http").Server(app);
-var io = require("socket.io")(server, {});
+var server = require('http').Server(app);
+var io = require('socket.io')(server, {});
+var rooms = [];
+
+for (i = 0; i < 30; i++) {
+  rooms.push(0);
+}
+
 server.listen(2000);
 
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/Client/index.html');
-})
-app.use('client', express.static(__dirname + '/Client'));
-console.log("finding clients");
+});
+
+app.use('/', express.static(__dirname + '/Client'));
+console.log('finding clients');
 
 io.sockets.on('connection', function(socket) {
-  console.log("Someone joined!");
-});
+    var roomNum;
+    console.log('joined');
+    for (i = 0; i < rooms.length; i++) {
+      if (rooms[i] < 2) {
+        rooms[i] += 1;
+        roomNum = i;
+        socket.join(i.toString());
+        if (rooms[i] == 1) {
+          socket.emit("firstJoined");
+        }
+        if (rooms[i] == 2) {
+          io.sockets.in(i.toString()).emit("start");
+        }
+        break;
+      }
+    }
+    console.log('added to room');
+    socket.on('angle', function(data) {
+      socket.broadcast.to(roomNum.toString()).emit('updateAngle', data);
+    })
+    socket.on('updateChar', function(data) {
+      console.log("lol");
+      socket.broadcast.to(roomNum.toString()).emit('updateEnemyChar', data);
+    })
+    socket.on('updateAttack', function(data) {
+      socket.broadcast.to(roomNum.toString()).emit('updateEnemyAttack', data);
+    })
+    socket.on('disconnect', function() {
+      rooms[roomNum] -= 1;
+    })
+  });
